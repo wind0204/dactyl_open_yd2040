26baf5a8522cb1983453e808d3963ae4973e7a05
Gunwoo Gim (il debole) <wind8702@gmail.com>
2024-12-23 09:04:59 +0900

    Assign KC_LEFT and KC_RGHT

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB ,   KC_Q   ,   KC_W   ,   KC_E   ,   KC_R   ,   KC_T   ,  _______ ,
         KC_CAPS,   KC_A   ,   KC_S   ,   KC_D   ,   KC_F   ,   KC_G   ,  KC_LBRC,
         KC_LSFT,   KC_Z   ,   KC_X   ,   KC_C   ,   KC_V   ,   KC_B   ,
-        _______,   _______,   _______,   _______,   KC_LALT,             KC_DEL , KC_MUTE,
+        _______,   _______,   KC_LEFT,   KC_RGHT,   KC_LALT,             KC_DEL , KC_MUTE,
                                                                KC_SPC ,  KC_LCTL, KC_LGUI,
         // right hand ( inversed )
         KC_EQL ,   KC_MINS,   KC_0   ,   KC_9   ,   KC_8   ,   KC_7   ,  KC_PGUP,
