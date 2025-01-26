1c947a8caf2418ac2ff16ae0d6a337a28380e73f
Gunwoo Gim (il debole) <wind8702@gmail.com>
2025-01-26 20:40:04 +0900

    Key rearrangement
    Add another OSL(THIRD_LAYER), another GUI, PAUS, LNG2(HANJ), SCROLL_LOCK
    Move ESC to the left side so that it's easy to press it while using a
    mouse on the right side

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -24,10 +24,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [QWERTY_LAYER] = LAYOUT_5x7(
         // left hand
         KC_GRAVE,  KC_1   ,   KC_2   ,   KC_3   ,   KC_4   ,   KC_5   ,  KC_6   ,
-        KC_TAB ,   KC_Q   ,   KC_W   ,   KC_E   ,   KC_R   ,   KC_T   ,  _______ ,
+        KC_TAB ,   KC_Q   ,   KC_W   ,   KC_E   ,   KC_R   ,   KC_T   ,  _______,
         KC_CAPS,   KC_A   ,   KC_S   ,   KC_D   ,   KC_F   ,   KC_G   ,  KC_LBRC,
         KC_LSFT,   KC_Z   ,   KC_X   ,   KC_C   ,   KC_V   ,   KC_B   ,
-        _______,   _______,   KC_LEFT,   KC_RGHT,   KC_LALT,             KC_DEL , KC_MUTE,
+        OSL(THIRD_LAYER), KC_ESC, KC_LEFT, KC_RGHT, KC_LALT,             KC_DEL , KC_MUTE,
                                                                KC_SPC ,  KC_LCTL, KC_LGUI,
         // right hand ( inversed )
         KC_EQL ,   KC_MINS,   KC_0   ,   KC_9   ,   KC_8   ,   KC_7   ,  KC_PGUP,
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_QUOT,   KC_SCLN,   KC_L   ,   KC_K   ,   KC_J   ,   KC_H   ,  KC_RBRC,
         KC_RSFT,   KC_SLSH,   KC_DOT ,   KC_COMM,   KC_M   ,   KC_N   ,
         OSL(THIRD_LAYER), KC_ENT, KC_END, KC_HOME,  KC_RALT,             KC_BSPC, KC_PSCR,
-                                                               KC_SPC ,  TD(TD_RCTL_L2), KC_ESC
+                                                               KC_SPC ,  TD(TD_RCTL_L2), KC_RGUI
     ),
 
     [SECOND_LAYER] = LAYOUT_5x7(
@@ -57,15 +57,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [THIRD_LAYER] = LAYOUT_5x7(
         // left hand
-        _______,   KC_F1  ,   KC_F2  ,   KC_F3  ,   KC_F4  ,   KC_F5  ,  KC_F6  ,
+        KC_SCROLL_LOCK, KC_F1, KC_F2 ,   KC_F3  ,   KC_F4  ,   KC_F5  ,  KC_F6  ,
         _______,   _______,   _______,   KC_UP  ,   _______,   _______,  _______,
         KC_NUM ,   _______,   KC_LEFT,   KC_DOWN,   KC_RGHT,   _______,  _______,
         _______,   _______,   _______,   _______,   _______,   _______,
-        _______,   _______,   _______,   _______,   _______,             _______, _______,
+        KC_LNG2,   _______,   _______,   _______,   _______,             _______, _______,
                                                                 _______, _______, _______,
         // right hand ( inversed )
         KC_F12 ,   KC_F11 ,   KC_F10 ,   KC_F9  ,   KC_F8  ,   KC_F7  ,  KC_INS ,
-        _______,   _______,   _______,   _______,   _______,   _______,  _______,
+        _______,   KC_PAUS,   _______,   _______,   _______,   _______,  _______,
         _______,   _______,   _______,   _______,   _______,   _______,  _______,
         _______,   _______,   _______,   _______,   _______,   _______,
         _______,   _______,   _______,   _______,   _______,             _______, _______,
