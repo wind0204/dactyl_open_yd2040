99fed79d54cd35de59f8c81db29f7fc7c9414b73
Gunwoo Gim (il debole) <wind8702@gmail.com>
2026-01-18 14:56:46 +0900

    I don't use the debugging purpose console messages

--- rev1/rules.mk
+++ rev1/rules.mk
@@ -1,7 +1,7 @@
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = yes         # Console for debug
+CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
